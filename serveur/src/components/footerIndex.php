635e40315d4213ef670222d70e89d576dbae5ae5
VAITOcr <kevinjec44@gmail.com>
2025-04-11 10:23:01 -0400

    Footer component maked

---
+++ serveur/src/components/footerIndex.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<footer class="py-3 bg-dark footer" id="contact">
+        <div class="container">
+            <div class="row">
+                <!-- Texte de copyright aligné à gauche -->
+                <div class="col-md-4 text-md-start text-center">
+                    <p class="text-white m-0">
+                        Copyright &copy; APVinc 2025. Tous droits réservés.  
+                    </p>
+                    <p class="text-white m-0">
+                        Toute reproduction, distribution ou modification sans autorisation préalable est strictement interdite.
+                    </p>
+                </div>
+
+                <!-- Contact au centre -->
+                <div class="col-md-4 text-md-center text-center">
+                    <p class="text-white m-0">Contactez-nous :</p>
+                    <p class="text-white m-0">contact@apvinc.com</p>
+                    <p class="text-white m-0">+1 514 514 5142</p>
+                </div>
+
+                <!-- Réseaux sociaux à droite -->
+                <div class="col-md-4 text-md-end text-center">
+                    <p class="text-white m-0">Suivez-nous :</p>
+                    <a href="#" class="me-2">
+                        <img src="client/images/facebook.png" alt="Facebook" width="40" >
+                    </a>
+                    <a href="#" class="me-2">
+                        <img src="client/images/twitter.png" alt="Twitter" width="40" >
+                    </a>
+                    <a href="#">
+                        <img src="client/images/linkedin.png" alt="LinkedIn" width="40" >
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
