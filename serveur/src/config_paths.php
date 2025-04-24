e1c566946f17d92753451ef37697807594ce0f33
VAITOcr <kevinjec44@gmail.com>
2025-04-24 16:30:22 -0400

    path v0.02

--- serveur/src/config_paths.php
+++ serveur/src/config_paths.php
@@ -1,12 +1,17 @@
 <?php
-
 $rootPath = realpath(__DIR__ . '/../../');
-
 $serveurPath = $rootPath . '/serveur/';
 $clientPath = $rootPath . '/client/';
 
 
-$projectName = basename($rootPath); 
-$serveurUrl = '/' . $projectName . '/serveur/';
-$clientUrl = '/' . $projectName . '/client/';
+$scriptName = $_SERVER['SCRIPT_NAME'];
+$realDir = str_replace('\\', '/', realpath(__DIR__ . '/../../'));
+$documentRoot = str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT']));
+$relativeUrl = str_replace($documentRoot, '', $realDir);
+if ($relativeUrl === '') $relativeUrl = '/';
+
+$baseUrl = rtrim($relativeUrl, '/') . '/';
+
+$serveurUrl = $baseUrl . 'serveur/';
+$clientUrl  = $baseUrl . 'client/';
 ?>
