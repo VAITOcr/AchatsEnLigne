2e96b459bc059ad3736b09f1d133d907d70af7fc
Adafang <adafang@proton.me>
2025-05-01 12:52:59 -0400

    Update env_vars.inc.php

--- serveur/src/includes/env/env_vars.inc.php
+++ serveur/src/includes/env/env_vars.inc.php
@@ -5,5 +5,5 @@
     define('BD', 'APVincdb');
     define('PEPPER', 'testestest');
 
-    const ERREUR_BD_CONNEXION = 'Vous avez une erreur de connexion au serveur de base de données';
+    const ERREUR_BD_CONNEXION = 'Vous avez une erreur de connexion au serveur de base de données !';
 ?>
