ac4faf64129cf26dd9c5bffd5f4847d89a204299
Adafang <adafang@proton.me>
2025-05-01 12:47:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['idm'], $_SESSION['role'])) {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-9">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Store APVinc</title>
